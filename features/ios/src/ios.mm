848026e0a3d5287b1c809649cc2e6e46714d0c82
Michael Kapelko <kornerr@gmail.com>
2018-08-31 12:54:43 +0300

    Build 01 for iOS

---
+++ features/ios/src/ios.mm
@@ -0,0 +1,47 @@
+
+/*
+This file is part of OGS Mahjong components:
+  https://github.com/OGStudio/ogs-mahjong-components
+
+Copyright (C) 2018 Opensource Game Studio
+
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+*/
+
+#include "ios.h"
+
+#include "library.h"
+
+// FEATURE AppDelegate/Properties
+    // FEATURE AppDelegate+HTTPClientProcessor-ios/Properties
+    // FEATURE AppDelegate+RenderVC/Properties
+// FEATURE AppDelegate/Setup
+    // FEATURE AppDelegate+RenderVC/Setup
+    // FEATURE AppDelegate+HTTPClientProcessor-ios/Setup
+// FEATURE AppDelegate/Impl
+    // FEATURE AppDelegate+HTTPClientProcessor-ios/Impl
+    // FEATURE AppDelegate+RenderVC/Impl
+// FEATURE AppDelegate/End
+
+// FEATURE HTTPClientProcessor-ios/Impl
+
+// FEATURE RenderVC/Frame
+    // FEATURE RenderVC+FrameReporting/Frame
+// FEATURE RenderVC/Impl
+    // FEATURE RenderVC+FrameReporting/Impl
+// FEATURE RenderVC/End
+
