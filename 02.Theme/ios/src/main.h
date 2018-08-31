58ca920d4bc6b733d8d412e5fdb870ad990162c1
Michael Kapelko <kornerr@gmail.com>
2018-08-31 12:59:00 +0300

    Build 02 for iOS

---
+++ 02.Theme/ios/src/main.h
@@ -0,0 +1,457 @@
+
+/*
+This file is part of OGS Mahjong components:
+  https://github.com/OGStudio/ogs-mahjong-components
+
+Copyright (C) 2018 Opensource Game Studio
+
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+*/
+
+#ifndef OGS_MAHJONG_COMPONENTS_MAIN_H
+#define OGS_MAHJONG_COMPONENTS_MAIN_H
+
+// Application+frame+Reporting Start
+#include "core.h"
+
+// Application+frame+Reporting End
+
+// Application+Logging Start
+#include "log.h"
+
+// Application+Logging End
+// Application+Rendering Start
+#include "render.h"
+
+#include <osgViewer/Viewer>
+#include <osgGA/TrackballManipulator>
+
+// Application+Rendering End
+
+// Example+Scene Start
+#include <osg/MatrixTransform>
+
+// Example+Scene End
+// Example+Theme Start
+#include "render.h"
+#include "resource.h"
+
+#include "ppl-theme.frag.h"
+#include "ppl-theme.vert.h"
+#include "tile-low.osgt.h"
+
+// Example+Theme End
+// Example+ThemeTest Start
+#include "tile-theme.png.h"
+#include "scene.h"
+#include <osg/MatrixTransform>
+
+// Example+ThemeTest End
+// Example+VBO Start
+#include "render.h"
+
+// Example+VBO End
+
+
+// OMC_MAIN_EXAMPLE_LOG Start
+#include "log.h"
+#include "format.h"
+#define OMC_MAIN_EXAMPLE_LOG_PREFIX "main::Example(%p) %s"
+#define OMC_MAIN_EXAMPLE_LOG(...) \
+    log::logprintf( \
+        OMC_MAIN_EXAMPLE_LOG_PREFIX, \
+        this, \
+        format::printfString(__VA_ARGS__).c_str() \
+    )
+// OMC_MAIN_EXAMPLE_LOG End
+
+// Example+StaticPluginOSG Start
+#include <osgDB/Registry>
+
+// Reference (statically) plugins to read `osgt` file.
+USE_OSGPLUGIN(osg2)
+USE_SERIALIZER_WRAPPER_LIBRARY(osg)
+// Example+StaticPluginOSG End
+// Example+StaticPluginPNG Start
+#include <osgDB/Registry>
+
+// Reference (statically) plugins to read `png` file.
+// Apple platforms use ImageIO. All others use libpng.
+#ifdef __APPLE__
+    USE_OSGPLUGIN(imageio)
+#else
+    USE_OSGPLUGIN(png)
+#endif
+// Example+StaticPluginPNG End
+
+namespace omc
+{
+namespace main
+{
+
+// Application Start
+class Application
+{
+    public:
+        Application(const std::string &name)
+        {
+
+// Application End
+            // Application+Logging Start
+            this->setupLogging(name);
+            
+            // Application+Logging End
+            // Application+Rendering Start
+            this->setupRendering();
+            
+            // Application+Rendering End
+// Application Start
+        }
+        ~Application()
+        {
+
+// Application End
+            // Application+Rendering Start
+            this->tearRenderingDown();
+            
+            // Application+Rendering End
+            // Application+Logging Start
+            this->tearLoggingDown();
+            
+            // Application+Logging End
+// Application Start
+        }
+
+// Application End
+    // Application+frame+Reporting Start
+    public:
+        core::Reporter frameReporter;
+        void frame()
+        {
+            this->viewer->frame();
+            this->frameReporter.report();
+        }
+    // Application+frame+Reporting End
+    // Application+setupWindow-ios Start
+    UIView *setupWindow(
+        int width,
+        int height,
+        float scale,
+        UIView *parentView
+    ) {
+        osgViewer::GraphicsWindowIOS *gc =
+            dynamic_cast<osgViewer::GraphicsWindowIOS *>(
+                render::createGraphicsContext(width, height, scale, parentView)
+            );
+        // Configure viewer's camera with FOVY and window size.
+        osg::Camera *cam = this->viewer->getCamera();
+        render::setupCamera(cam, gc, 30, width * scale, height * scale);
+        // Return UIView for embedding.
+        return (UIView *)gc->getView();
+    }
+    // Application+setupWindow-ios End
+
+    // Application+Logging Start
+    private:
+        log::Logger *logger;
+        void setupLogging(const std::string &appName)
+        {
+            // Create custom logger.
+            this->logger = new log::Logger(appName);
+            // Provide the logger to OpenSceneGraph.
+            osg::setNotifyHandler(this->logger);
+            // Only accept notifications of Info level or higher
+            // like warnings and errors.
+            //osg::setNotifyLevel(osg::INFO);
+            osg::setNotifyLevel(osg::WARN);
+        }
+        void tearLoggingDown()
+        {
+            // Remove the logger from OpenSceneGraph.
+            // This also destroys the logger: no need to deallocate it manually.
+            osg::setNotifyHandler(0);
+        }
+    // Application+Logging End
+    // Application+Rendering Start
+    public:
+        void setScene(osg::Node *scene)
+        {
+            // Make sure we reset the scene upon setting the same scene again.
+            this->viewer->setSceneData(0);
+            this->viewer->setSceneData(scene);
+        }
+    private:
+        osgViewer::Viewer *viewer;
+        void setupRendering()
+        {
+            // Create OpenSceneGraph viewer.
+            this->viewer = new osgViewer::Viewer;
+            // Use single thread: CRITICAL for mobile and web.
+            this->viewer->setThreadingModel(osgViewer::ViewerBase::SingleThreaded);
+            // Create manipulator: CRITICAL for mobile and web.
+            this->viewer->setCameraManipulator(new osgGA::TrackballManipulator);
+        }
+        void tearRenderingDown()
+        {
+            delete this->viewer;
+        }
+    // Application+Rendering End
+// Application Start
+};
+// Application End
+
+// Example+02 Start
+const auto EXAMPLE_TITLE = "OMC-02: Theme";
+// Example+02 End
+
+// Example Start
+struct Example
+{
+    Application *app;
+
+    typedef std::map<std::string, std::string> Parameters;
+
+    Example(const Parameters &parameters)
+    {
+        this->app = new Application(EXAMPLE_TITLE);
+
+// Example End
+        // Example+Scene Start
+        this->setupScene();
+        
+        // Example+Scene End
+        // Example+Theme Start
+        this->setupTheme();
+        
+        // Example+Theme End
+        // Example+ThemeTest Start
+        this->setupThemeTest();
+        
+        // Example+ThemeTest End
+        // Example+VBO Start
+        this->setupSceneVBO();
+        
+        // Example+VBO End
+// Example Start
+    }
+    ~Example()
+    {
+
+// Example End
+        // Example+Theme Start
+        this->tearThemeDown();
+        
+        // Example+Theme End
+// Example Start
+        delete this->app;
+    }
+
+// Example End
+    // Example+Scene Start
+    private:
+        osg::ref_ptr<osg::MatrixTransform> scene;
+        const std::string sceneSetupCallbackName = "SceneSetup";
+    
+        void setupScene()
+        {
+            this->scene = new osg::MatrixTransform;
+    
+            // Provide scene to application after the first frame
+            // to let other components configure scene prior that event.
+            this->app->frameReporter.addCallback(
+                [&] {
+                    this->app->setScene(this->scene);
+                    // Unsubscribe from the rest of frame reports.
+                    this->app->frameReporter.removeCallback(
+                        this->sceneSetupCallbackName
+                    );
+                },
+                this->sceneSetupCallbackName
+            );
+        }
+    // Example+Scene End
+    // Example+Theme Start
+    private:
+        render::TileTheme *theme;
+        // Theme texture configuration.
+        const osg::Vec2 textureSize = {1024, 2048};
+        const osg::Vec2 tileFaceSize = {160, 240};
+        // Theme model.
+        const render::TileTheme::Indices faceIndices = {15, 23, 16, 17};
+        osg::ref_ptr<osg::Geode> tileModel;
+        // Materials for normal and selected states of tiles.
+        osg::ref_ptr<osg::StateSet> themeMaterial;
+        osg::ref_ptr<osg::StateSet> themeMaterialSelected;
+    
+        void setupTheme()
+        {
+            this->theme =
+                new render::TileTheme(
+                    this->textureSize,
+                    this->tileFaceSize,
+                    this->faceIndices
+                );
+            this->setupTileModel();
+            this->setupMaterials();
+        }
+        void tearThemeDown()
+        {
+            delete this->theme;
+        }
+        void setupTileModel()
+        {
+            resource::Resource tileResource(
+                "models",
+                "tile-low.osgt",
+                tile_low_osgt,
+                tile_low_osgt_len
+            );
+            auto node = resource::node(tileResource);
+            auto model = reinterpret_cast<osg::Geode *>(node.get());
+            // Make sure model is valid.
+            if (!model)
+            {
+                OMC_MAIN_EXAMPLE_LOG(
+                    "ERROR Could not setup tile model '%s/%s'",
+                    tileResource.group.c_str(),
+                    tileResource.name.c_str()
+                );
+                return;
+            }
+            
+            this->tileModel = model;
+        }
+        void setupMaterials()
+        {
+            // Create resources.
+            resource::Resource shaderFrag(
+                "shaders",
+                "ppl-theme.frag",
+                ppl_theme_frag,
+                ppl_theme_frag_len
+            );
+            resource::Resource shaderVert(
+                "shaders",
+                "ppl-theme.vert",
+                ppl_theme_vert,
+                ppl_theme_vert_len
+            );
+    
+            // Create shader program.
+            auto prog =
+                render::createShaderProgram(
+                    resource::string(shaderVert),
+                    resource::string(shaderFrag)
+                );
+    
+            // Create normal state material.
+            auto normal = new osg::StateSet;
+            normal->setAttribute(prog);
+            normal->addUniform(new osg::Uniform("image", 0));
+            normal->addUniform(new osg::Uniform("isSelected", false));
+            this->themeMaterial = normal;
+    
+            // Create selected state material.
+            auto selected = new osg::StateSet;
+            selected->setAttribute(prog);
+            selected->addUniform(new osg::Uniform("image", 0));
+            selected->addUniform(new osg::Uniform("isSelected", true));
+            this->themeMaterialSelected = selected;
+        }
+    // Example+Theme End
+    // Example+ThemeTest Start
+    private:
+        osg::ref_ptr<osg::MatrixTransform> tileScene;
+        void setupThemeTest()
+        {
+            // Create tile scene to host tiles.
+            this->tileScene = new osg::MatrixTransform;
+            this->scene->addChild(this->tileScene);
+            // Apply normal state material to the whole scene.
+            this->tileScene->setStateSet(this->themeMaterial);
+    
+            // Rotate the tile scene to have a better view.
+            scene::setSimpleRotation(this->tileScene, {60, 0, 0});
+    
+            // Configure tile.
+            this->theme->setFaceId(0, this->tileModel);
+            // Add it to the scene.
+            this->tileScene->addChild(this->tileModel);
+    
+            // Create another tile.
+            auto leftTile = new osg::Geode(*this->tileModel, osg::CopyOp::DEEP_COPY_ALL);
+            // Configure it.
+            this->theme->setFaceId(6, leftTile);
+            // Move it to the left.
+            auto leftTransform = new osg::MatrixTransform;
+            leftTransform->addChild(leftTile);
+            scene::setSimplePosition(leftTransform, {-3, 0, 0});
+            // Add it to the scene.
+            this->tileScene->addChild(leftTransform);
+            // Assign selected state material to the left tile.
+            leftTile->setStateSet(this->themeMaterialSelected);
+    
+            // Create one more tile.
+            auto rightTile = new osg::Geode(*this->tileModel, osg::CopyOp::DEEP_COPY_ALL);
+            // Configure it.
+            this->theme->setFaceId(3, rightTile);
+            // Move it to the right.
+            auto rightTransform = new osg::MatrixTransform;
+            rightTransform->addChild(rightTile);
+            scene::setSimplePosition(rightTransform, {3, 0, 0});
+            // Add it to the scene.
+            this->tileScene->addChild(rightTransform);
+    
+            // Set texture to materials.
+            resource::Resource texRes(
+                "textures",
+                "tile-theme.png",
+                tile_theme_png,
+                tile_theme_png_len
+            );
+            auto texture = resource::createTexture(texRes);
+            this->themeMaterial->setTextureAttributeAndModes(0, texture);
+            this->themeMaterialSelected->setTextureAttributeAndModes(0, texture);
+        }
+    // Example+ThemeTest End
+    // Example+VBO Start
+    private:
+        void setupSceneVBO()
+        {
+            // Do nothing for an empty scene.
+            if (!this->scene.valid())
+            {
+                return;
+            }
+            // Use VBO and EBO instead of display lists.
+            // CRITICAL for:
+            // * mobile
+            // * web (Emscripten) to skip FULL_ES2 emulation flag
+            render::VBOSetupVisitor vbo;
+            this->scene->accept(vbo);
+        }
+    // Example+VBO End
+ 
+// Example Start
+};
+// Example End
+
+} // namespace main
+} // namespace omc
+
+#endif // OGS_MAHJONG_COMPONENTS_MAIN_H
+
