93d053e8b21acba503f56fcb3ced1be786af1158
Michael Kapelko <kornerr@gmail.com>
2018-09-11 12:47:06 +0300

    Print css element size

--- 04.SetLayoutThemeSeed/web/src/main.h
+++ 04.SetLayoutThemeSeed/web/src/main.h
@@ -435,10 +435,6 @@ struct Example
         this->setupTheme();
         
         // Example+Theme End
-        // Example+SetLayoutThemeSeedTest Start
-        this->setupSetLayoutThemeSeedTest();
-        
-        // Example+SetLayoutThemeSeedTest End
         // Example+VBO Start
         this->setupSceneVBO();
         
@@ -449,10 +445,6 @@ struct Example
     {
 
 // Example End
-        // Example+SetLayoutThemeSeedTest Start
-        this->tearSetLayoutThemeSeedTestDown();
-        
-        // Example+SetLayoutThemeSeedTest End
         // Example+Theme Start
         this->tearThemeDown();
         
@@ -709,101 +701,6 @@ struct Example
             this->parameters = parameters;
         }
     // Example+Parameters End
-    // Example+SetLayoutThemeSeedTest Start
-    private:
-        core::Sequence setupSequence;
-        void setupSetLayoutThemeSeedTest()
-        {
-            this->setupInternalLayouts();
-            this->setupInternalThemes();
-            this->setupDefaultLayoutTheme();
-    
-            this->setupSequence.setActions({
-                "loadLayout",
-                "loadTheme",
-                "finishSetup",
-            });
-    
-            // Register actions.
-            CORE_REGISTER_SEQUENCE_ACTION(
-                this->setupSequence,
-                "loadLayout",
-                this->loadLayout()
-            );
-            CORE_REGISTER_SEQUENCE_ACTION(
-                this->setupSequence,
-                "loadTheme",
-                this->loadTheme()
-            );
-            CORE_REGISTER_SEQUENCE_ACTION(
-                this->setupSequence,
-                "finishSetup",
-                this->finishSetup()
-            );
-    
-            // Enable sequence.
-            this->setupSequence.setEnabled(true);
-        }
-        void tearSetLayoutThemeSeedTestDown()
-        {
-            this->tearInternalLayoutsDown();
-            //this->tearNodeSelectionDown();
-        }
-        core::Reporter *loadLayout()
-        {
-            // Do nothing if `layout` parameter is absent.
-            auto it = this->parameters.find("layout");
-            if (it == this->parameters.end())
-            {
-                return 0;
-            }
-    
-            // Load layout otherwise.
-            auto value = it->second;
-            return this->loadLayout(value);
-        }
-        core::Reporter *loadTheme()
-        {
-            // Do nothing if `theme` parameter is absent.
-            auto it = this->parameters.find("theme");
-            if (it == this->parameters.end())
-            {
-                return 0;
-            }
-    
-            // Load theme otherwise.
-            auto value = it->second;
-            return this->loadTheme(value);
-        }
-        core::Reporter *finishSetup()
-        {
-            this->setupTiles();
-            this->setupNodeSelection();
-            this->setupTileSelection();
-            this->setupTileSelectionDepiction();
-            this->setupTileMatching();
-            this->setupUnmatchedTilesDeselection();
-            this->setupMatchedTilesRemoval();
-            this->setupGameState();
-    
-            return 0;
-        }
-        void setupTiles()
-        {
-            // By default, use seed of the current time.
-            int seed = time(0);
-    
-            // Override it with the seed coming from parameters.
-            auto it = this->parameters.find("seed");
-            if (it != this->parameters.end())
-            {
-                seed = atoi(it->second.c_str());
-                MAIN_EXAMPLE_LOG("Using seed '%d'", seed);
-            }
-    
-            this->setupTiles(seed);
-        }
-    // Example+SetLayoutThemeSeedTest End
     // Example+Scene Start
     private:
         osg::ref_ptr<osg::MatrixTransform> scene;
