db6891fc97cb66c9854c911886dcce3cc4e38cb0
Michael Kapelko <kornerr@gmail.com>
2018-10-23 10:56:09 +0300

    Move resource::isPathRemote to OSGCPE

--- features/resource/isPathRemote.cpp
+++
@@ -1,9 +0,0 @@
-FEATURE resource.h/Impl
-bool isPathRemote(const std::string &path)
-{
-    return
-        format::stringStartsWith(path, "http://") ||
-        format::stringStartsWith(path, "https://")
-        ;
-}
-
